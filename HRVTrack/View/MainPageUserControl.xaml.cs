a359bd86bd2e3a62f3d2a7038e1b682a05150439
unknown <olfuxa@gmail.com>
2025-10-22 12:02:36 +0200

    Utworzenie view

---
+++ HRVTrack/View/MainPageUserControl.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace HRVTrack.View
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainPageUserControl.xaml
+    /// </summary>
+    public partial class MainPageUserControl : UserControl
+    {
+        public MainPageUserControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ HRVTrack/View/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using HRVTrack.View;
+namespace HRVTrack
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MainPageButton_Click(object sender, RoutedEventArgs e)
+        {
+            MainContent.Content = new MainPageUserControl();
+        }
+
+        private void MeasurmentsPageButton_Click(object sender, RoutedEventArgs e)
+        {
+            MainContent.Content = new MeasurmentsPageUserControl();
+        }
+
+        private void StatsPageButton_Click(object sender, RoutedEventArgs e)
+        {
+            MainContent.Content = new StatsPageUserControl();
+        }
+
+        private void SettingsPageButton_Click(object sender, RoutedEventArgs e)
+        {
+            MainContent.Content = new SettingsPageUserControl();
+        }
+    }
+}
