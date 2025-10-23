f81f417e673c20fb680d7ef91e18d922332f5857
unknown <olfuxa@gmail.com>
2025-10-23 08:52:58 +0200

    Definicja modelu

---
+++ HRVTrack/Model/HrvDataModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRVTrack.Model
+{
+    public class HrvDataModel
+    {
+        public DateTime TimeStamp { get; set; }
+        public double HrvValue { get; set; } // RMSSD
+        public int HeartRate { get; set; } 
+        public double Readiness { get; set; } // 0.00 - 100.0
+        public int PhysiologicalAge { get; set; }
+        public double MeanRR { get; set; } // xxx.xx (ms)
+
+    }
+}
---
+++ HRVTrack/ViewModel/Helpers/DatabaseHelper.cs
@@ -0,0 +1,80 @@
+﻿using System.Data.SQLite;
+using System.Linq.Expressions;
+using System.Windows;
+namespace HRVTrack.ViewModel.Helpers
+{
+    public class DatabaseHelper
+    {
+        public static void InitializeDatabase()
+        {
+            try
+            {
+                using (var connection = new SQLiteConnection("Data Source = AppData.db;Version=3;"))
+                {
+                    connection.Open();
+                    var cmd = connection.CreateCommand();
+                    cmd.CommandText = $"CREATE TABLE HrvData (\r\n    TimeStamp TEXT NOT NULL,              -- DateTime jako tekst w formacie ISO 8601\r\n    HrvValue REAL NOT NULL,               -- RMSSD\r\n    HeartRate INTEGER NOT NULL,           -- Tętno\r\n    Readiness REAL NOT NULL,              -- 0.00 - 100.0\r\n    PhysiologicalAge INTEGER NOT NULL,    -- Wiek fizjologiczny\r\n    MeanRR REAL NOT NULL                  -- Średni RR w ms\r\n);";
+                    cmd.ExecuteNonQuery();
+
+
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Błąd inicjalizacji bazy daynch:\n{ex.Message}", "Błąd!", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+        public static bool ExecuteNonQuery(string query)
+        {
+            try
+            {
+                using (var connection = new SQLiteConnection("Data Source = AppData.db;Version=3;"))
+                {
+                    connection.Open();
+                    var cmd = connection.CreateCommand();
+                    cmd.CommandText = query;
+                    cmd.ExecuteNonQuery();
+                    return true;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Błąd zapisywania do bazy danych:\n{ex.Message}", "Błąd!", MessageBoxButton.OK, MessageBoxImage.Error);
+                return false;
+            }
+        }
+
+        public static List<Dictionary<string, object>> ExecuteQuery(string query)
+        {
+            var results = new List<Dictionary<string, object>>();
+            try
+            {
+                using (var connection = new SQLiteConnection("Data Source = AppData.db;Version=3;"))
+                {
+                    connection.Open();
+                    using (var command = new SQLiteCommand(query, connection))
+                    {
+                        using (var reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                var row = new Dictionary<string, object>();
+                                for (int i = 0; i < reader.FieldCount; i++)
+                                {
+                                    row[reader.GetName(i)] = reader.GetValue(i);
+                                }
+                                results.Add(row);
+                            }
+                        }
+                    }
+
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Błąd odczytu bazy danych:\n{ex.Message}", "Błąd!", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            return results;
+        }
+    }
+}
